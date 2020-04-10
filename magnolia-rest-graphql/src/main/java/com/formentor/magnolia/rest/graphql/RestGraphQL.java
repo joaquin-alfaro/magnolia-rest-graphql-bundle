3b73fc1a201dcba791a372b2db085b750bcc8f4f
Joaquin <joaquin.alfaro@gmail.com>
2020-04-10 18:19:44 +0200

    Scaffold of the magnolia bundle

---
+++ magnolia-rest-graphql/src/main/java/com/formentor/magnolia/rest/graphql/RestGraphQL.java
@@ -0,0 +1,13 @@
+package com.formentor.magnolia.rest.graphql;
+
+/**
+ * This class is optional and represents the configuration for the magnolia-rest-graphql module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/magnolia-rest-graphql</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class RestGraphQL {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
